9d10fc2133f8eed29a4b5c69c74f51ec53e79cbc
Liuver C. Espinosa <liuver.carrera.espinosa@gmail.com>
2023-01-21 19:35:23 -0500

    first

--- src/main/java/org/example/StringBuilderMain.java
+++ src/main/java/org/example/StringBuilderMain.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class StringBuilderMain {
+
+    public static void main(String[] args) {
+
+        String palindrome = "abcdba";
+
+        System.out.println(StringBuilderMain.isPalindrome(palindrome));
+
+    }
+
+    public static boolean isPalindrome(String str) {
+
+        str = str.toLowerCase();
+
+        int halfWay = str.length() / 2;
+        int endCursor;
+        for (int startCursor = 0; startCursor < halfWay; startCursor++) {
+            endCursor = str.length() - startCursor - 1;
+            if (str.charAt(endCursor) != str.charAt(startCursor)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
